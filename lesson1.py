1e783ab35f8b368fafa3dce8427255b2620c91b4
MichaelSwanRu <Michael.Swan.Ru@gmail.com>
2022-01-19 12:27:42 +0300

    add lesson codes

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,35 @@
-my first code
+st1 = 'мама мыла раму!'
+st2 = "папа мыл раму"
+st3 = st1 + ' '  + st2
+st4 = '12'
+st1[3]
+
+
+i1 = 10
+i2 = 11
+i3 = i1 + i2
+
+i1 == 11
+
+j1 = 10.0
+j2 = 11
+j3 = j1 + j2
+
+j4 = int(j3)
+
+
+st4 = '12'
+j5 = int(st4)
+
+v = [1, 1.2, '3.1415', 3.1415, 'text']
+v[3]
+
+w = [x for x in range(100)]
+w1 =  [x for x in w if x >= 10]
+
+for i in range(len(v)):
+    print(v[i])
+
+
+37 // 6
+37 % 6
